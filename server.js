964a9df785166901a7275ba50705b33523066a55
last-hop <amansingh.28757@gmail.com>
2025-01-12 02:32:36 +0530

    mongodd

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const cors = require('cors');
+const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
-const app = express();
 
 dotenv.config();
 
-const mongoURI = process.env.MONGO_URI
+const app = express();
+
+// MongoDB Connection
+const mongoURI = process.env.MONGO_URI;
 mongoose
-  .connect(mongoURI)
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-app.use(express.json())
+// Middleware
+app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Export the app for Vercel
+module.exports = app;
