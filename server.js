e6d78da0b484110446a7ba0502ef82326c0f46c2
last-hop <amansingh.28757@gmail.com>
2025-01-12 02:21:21 +0530

    mongo

--- server.js
+++ server.js
@@ -5,26 +5,18 @@ const cors = require('cors');
 const userRoutes = require("./routes/userRoutes");
 const app = express();
 
-
-
-
 dotenv.config();
 
-
-const mongoURI = "mongodb://localhost:27017";
+const mongoURI = process.env.MONGO_URI
 mongoose
   .connect(mongoURI)
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-
 app.use(express.json())
-
-app.use(cors({ origin: 'http://localhost:3000' })); ;
 
 // Routes
 app.use("/api/users", userRoutes);
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
